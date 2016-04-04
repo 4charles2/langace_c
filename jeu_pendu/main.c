8a62a6f92755e836d1d3830661b15b5098e21d92
4charles2 <ctognol@laposte.net>
2016-04-04 13:57:01 +0200

    fini je de base pendu

--- jeu_pendu/main.c
+++ jeu_pendu/main.c
@@ -1,41 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
-
 #include "main.h"
 
-int main(){
+int main(void){
     printf("Bienvenue dans mon jeu du pendu\n");
-    game_pendu();
+    ft_game_pendu();
     return 0;
 }
 
-void game_pendu()
+void ft_game_pendu()
 {
     st_game party;
-    if(creation_mystery_word(&party))
-        printf("%s", party.mot_mystere);
-    else
-        printf("Le mot mystere n'à pas été créer");
+	party.nb_coup_restant = 15;
+
+	if( (party.nb_joueur = ft_choice_number_gamer()) == 2 )
+	{
+		printf("Ecrivez le mot a deviner :\n");
+		scanf("%s", party.mot_mystere);
+		def_videBuffer();
+		ft_upper_string(&party);
+	}
+	else
+		ft_draw_word_dico(&party);
+
+	ft_resolution_game(&party);
+
+	printf("%s", party.mot_mystere);
 }
 
-int creation_mystery_word(st_game* party)
+int ft_choice_number_gamer()
 {
-    FILE* dico = NULL;
+	int choix = 0;
 
-    srand(time(NULL));
-    int number_rand = (rand()%8);
-    printf("%d", number_rand);
-    //fseek(dico, number_rand, SEEK_SET);
-    dico = fopen("dico.txt", "r");
-    if(dico == NULL)
-        printf("Le dictionnaire ne c'est pas ouvert");
-    if(fgets(party->mot_mystere, 30, dico))
-    {
-        fclose(dico);
-        return 1;
-    }
-    else
-        return 0;
+	printf("Choisissez le nombre de joueur\n1.Un joueur\n2.Deux joueur\nEntrez un choix(1 ou 2) : ");
+
+	scanf("%d", &choix);
+	def_videBuffer();
+	
+	switch(choix)
+	{
+	case 1:
+		return 1;
+		break;
+	case 2:
+		return 2;
+		break;
+	default :
+		printf("Vous n'avez pas entré un bon choix recommencé\n");
+		ft_choice_number_gamer();
+		break;
+	}
+	return 1;
 }
