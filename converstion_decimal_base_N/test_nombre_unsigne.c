ff5cb22a0a2e1a9d2841f977283e27eec4b0f508
4charles2 <ctognol@laposte.net>
2016-07-09 12:35:45 +0200

    erreur coriger test_nombre_signe et creation test nombre unsigned

--- converstion_decimal_base_N/test_nombre_unsigne.c
+++ converstion_decimal_base_N/test_nombre_unsigne.c
@@ -4,14 +4,12 @@
 //On depasse la valeur maximum pour la variable Soit 128 pour char qui peut aller jusqu'a la valeur 127 mais qui donne 128 résultat avce le zero
 int main (void)
 {
-	char k = 0;
+	unsigned char k = 0;
 	do
 	{
 		printf("%d\n", k);
 		k++;
 	}while(k>=0);
-	{
-		printf("%d\n", k);
-	}
+	printf("%d\n", k);
 	return EXIT_SUCCESS;
 }
